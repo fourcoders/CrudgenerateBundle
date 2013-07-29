b8f046de869d9168a9c9ca9d027a6b854e760782
juanber <juanber84@gmail.com>
2013-07-29 20:11:16 +0200

    Extendido command sensio ya coje nuestras plantillas

--- Command/FourcodersGeneratorCommand.php
+++ Command/FourcodersGeneratorCommand.php
@@ -1,16 +1,37 @@
 <?php
 namespace Fourcoders\Bundle\CrudgenerateBundle\Command;
+
+use Sensio\Bundle\GeneratorBundle\Generator\DoctrineCrudGenerator;
 use Sensio\Bundle\GeneratorBundle\Command\GenerateDoctrineCrudCommand;
- 
+use Fourcoders\Bundle\CrudgenerateBundle\Generator\DoctrineCrudGeneratorCustom;
+
+
 class FourcodersGeneratorCommand extends GenerateDoctrineCrudCommand
 {
+
     protected $generator;
- 
+
     protected function configure()
     {
         parent::configure();
- 
+
         $this->setName('fourcoders:generate:crud');
-        $this->setDescription('Fourcoders admin generator');
+        $this->setDescription('My Crud generate');
+
     }
+
+    protected function getGenerator($bundle = null)
+    {
+        $skeletonDirs[] = __DIR__.'/../Resources/skeleton';
+        $skeletonDirs[] = __DIR__.'/../Resources';
+      //  if (null === $this->generator) {
+            $this->generator = $this->createGenerator();
+            $this->generator->setSkeletonDirs($skeletonDirs);
+       // }
+
+        return $this->generator;
+    }
+
+
 }
+
